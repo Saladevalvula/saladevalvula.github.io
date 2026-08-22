832a81ec640f7ca359eda5891330877aa0677e0d
wallacegoulart2020-lab <wallacegoulart2020@gmail.com>
2026-08-21 22:38:09 -0300

    fix: declarar todos os bindings globais CMMS V2

--- cmms-v2-prelude.js
+++ cmms-v2-prelude.js
@@ -1,8 +1,22 @@
 /* Compatibilidade de bindings globais para a camada CMMS V2.
- * O app legado já declara a maior parte das funções sobrescritas. Estas três
- * telas são novas na V2 e precisam existir como bindings antes do script
- * principal executar em strict mode.
+ * A V2 substitui funções do app legado e também cria novas telas. Como o
+ * script principal roda em strict mode, declaramos explicitamente aqui todos
+ * os bindings globais que ele sobrescreve/cria.
  */
+var setMode;
+var render;
+var renderRegisterFlow;
+var doSubmit;
+var submitSonda;
+var submitFalhaGeral;
+var askDel;
+var getFilteredData;
+var renderHistoryView;
+var loadHistory;
+var doOpDone;
+var askOpDone;
+var askDelOp;
 var renderOrdersView;
 var renderWorkOrdersView;
 var renderDashboardView;
+var openManual;
